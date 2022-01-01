2b8f96179bf7d7d03ad190ff0cf5ba306dd644d2
Abhishek Sharma <abhisheksharma1123@gmail.com>
2021-12-31 20:02:17 -0800

    final-hulu-code

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header className="h-auto flex flex-col sm:flex-row m-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collection" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
+      </div>
+      <Image
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt="image"
+        className="object-contain"
+      />
+    </header>
+  );
+}
+
+export default Header;
